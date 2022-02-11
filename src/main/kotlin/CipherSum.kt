f69e2e20e6e00005544398908002d07ff2e585d7
Ján Forgáč ml <jforgac04@gmail.com>
2022-02-11 08:38:28 +0100

    .

--- src/main/kotlin/CipherSum.kt
+++
@@ -1,8 +0,0 @@
-fun solve(num: String, acc: Int = 0): Int{
-	if(num.length == 1) return num[0].digitToInt()
-	return num[0].digitToInt() + solve(num.drop(1),acc)
-}
-
-fun main(){
-	println(solve("1954"))
-}
